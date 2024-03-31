34a8649267f1d82d2a970caa0f756410ae845d46
robot0223 <fydy0223@gmail.com>
2024-03-31 19:56:46 +0900

    added home/about page and header and connected /home to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+  get '/home' => 'home#index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
